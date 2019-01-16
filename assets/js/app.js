10e8d11ffad19666692e60187ce268831e9dcec7
aincatoni <ain.catoni@gmail.com>
2019-01-16 20:54:46 -0300

    subiendo cambios 20:54

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,25 @@
 // Function ready
 $(function (){
+var url = 'https://api.instagram.com/oembed?url=https://www.instagram.com/p/BpMiaXVFh4U/' 
+
+  $.ajax ({
+    url: url,
+    method: 'GET',
+  }).then(function (data){
+    console.log(data);
+    $(".instagram").append(data.html);
+
+    });
 
 
+    $('#links').on('click',function (event) {
+      event = event || window.event;
+      var target = event.target || event.srcElement,
+          link = target.src ? target.parentNode : target,
+          options = {index: link, event: event},
+          links = this.getElementsByTagName('a');
+      blueimp.Gallery(links, options);
+  });
+
+  
 });
